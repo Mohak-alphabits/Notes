f37a81b771f4a6027a9d3bf8a7aef2d1284f26cd
Mohak <mohak@ealphabits.com>
2025-01-13 19:28:36 +0530

    Commit untracked files before pulling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -16,3 +17,29 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+const express = require("express");
+const userRoutes = require("../Routes/userRoutes");
+const connectMongoose = require("../database/dbConnect")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+require('dotenv').config()
+connectMongoose()
+const app = express();
+app.use(cookieParser());
+const port = 5000;
+
+app.use(express.json());
+  app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+)
+app.use("/api", userRoutes);
+
+app.listen(port, () => {
+  console.log('Server running');
+});
+>>>>>>> 8540135 (Commit untracked files before pulling)
